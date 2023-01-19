6fbc00a798b85e2d2d9b343bb2fe0ecf29a132d5
Pierangelo Guerrero <pierangelo.guerrero@gmail.com>
2023-01-19 22:02:33 +0100

    insertData function

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -20,87 +20,89 @@
       type="text"
       placeholder="Cook a lasagna"
     />
-    <input
-      class="bg-blue-800 hover:bg-blue-500 text-white w-1/4 font-medium py-4 px-16 rounded-lg ml-5 mb-8"
-      type="submit"
-      value="Add Task"
-    />
+    <button @click="addTask" class="bg-blue-800 hover:bg-blue-500 text-white w-1/4 font-medium py-4 px-16 rounded-lg ml-5 mb-8">Add Task
+
+    </button>
+     
   </div>
 
   <h1 class="text-4xl text-red-600 text-center mb-10">ToDo List:</h1>
 
   <div class="flex flex-row">
     <div
-      class="bg-yellow-200 text-black py-10 px-6 rounded-lg w-1/6 focus:outline-none focus:shadow-outline ml-5 mb-5 flex justify-around"
+      class="bg-yellow-200 text-black py-10 px-6 rounded-lg w-1/4 focus:outline-none focus:shadow-outline ml-5 mb-5 flex justify-start flex-wrap  "
     >
-      <label>
-        <input type="checkbox" class="ml-8" />
-      </label>
+    <button
+          class="bg-slate-400 hover:bg-slate-300 text-white rounded-lg py-1 px-2 mr-8"
+        >
+          Edit
+        </button>
 
-      <div>
+      
         <input
           type="text"
           placeholder="Task"
           class="bg-yellow-200 text-center"
         />
-      </div>
+      
 
-      <div>
+      
         <button
-          class="bg-red-600 hover:bg-red-500 text-white rounded-lg py-1 px-2 mr-8"
+          class="bg-red-600 hover:bg-red-500 text-white rounded-lg py-1 px-2"
         >
           Delete
         </button>
+      
+      <div class=" mt-10">
+        
+        <button
+          class=" bg-blue-900 hover:bg-blue-700 text-cyan-300 rounded-lg py-1 px-2"
+        >
+          Did It
+        </button>
+
       </div>
+
+      
     </div>
 
     <div
-      class="bg-yellow-200 text-black py-10 px-6 rounded-lg w-1/6 focus:outline-none focus:shadow-outline ml-5 mb-5 flex justify-around"
+      class="bg-yellow-200 text-black py-10 px-6 rounded-lg w-1/4 focus:outline-none focus:shadow-outline ml-5 mb-5 flex justify-start flex-wrap  "
     >
-      <label>
-        <input type="checkbox" class="ml-8" />
-      </label>
+    <button
+          class="bg-slate-400 hover:bg-slate-300 text-white rounded-lg py-1 px-2 mr-8"
+        >
+          Edit
+        </button>
 
-      <div>
+      
         <input
           type="text"
           placeholder="Task"
           class="bg-yellow-200 text-center"
         />
-      </div>
+      
 
-      <div>
+      
         <button
-          class="bg-red-600 hover:bg-red-500 text-white rounded-lg py-1 px-2 mr-8"
+          class="bg-red-600 hover:bg-red-500 text-white rounded-lg py-1 px-2"
         >
           Delete
         </button>
-      </div>
-    </div>
-
-    <div
-      class="bg-yellow-200 text-black py-10 px-6 rounded-lg w-1/6 focus:outline-none focus:shadow-outline ml-5 mb-5 flex justify-around"
-    >
-      <label>
-        <input type="checkbox" class="ml-8" />
-      </label>
-
-      <div>
-        <input
-          type="text"
-          placeholder="Task"
-          class="bg-yellow-200 text-center"
-        />
-      </div>
-
-      <div>
+      
+      <div class=" relative mt-10">
         <button
-          class="bg-red-600 hover:bg-red-500 text-white rounded-lg py-1 px-2 mr-8"
+          class=" bg-orange-500 hover:bg-orange-400 text-white rounded-lg py-1 px-2 bottom-0  left-0"
         >
-          Delete
+          Pending
         </button>
+       
+
       </div>
+
+      
     </div>
+
     
   </div>
 </template>
@@ -108,14 +110,25 @@
 <script setup>
 import { supabase } from "../supabase";
 import { useRouter } from "vue-router";
+import { useTaskStore } from '../store/task';
 
 const router = useRouter();
+
+const task = useTaskStore();
 
 const logout = async () => {
   console.log("data");
   await supabase.auth.signOut();
   router.push({ name: "Auth" });
 };
+
+const addTask = async () => {
+  await task.insert();
+}
+
+const fetchTasks = async () => {
+  await task.fetchTasks();
+}
 </script>
 
 <style scoped></style>
--- src/store/task.js
+++ src/store/task.js
@@ -15,7 +15,12 @@ export const useTaskStore = defineStore("tasks", {
         .order("id", { ascending: false });
       this.tasks = tasks;
     },
-    // Hacer POST
+    async insert() {
+    const { data: tasks } = await supabase
+  .from('tasks')
+  .insert({ user_id: 'dfcec423-af5e-44c9-9c79-25989e13e087', title: 'new task' })
+  
+},  
     // Hacer el PUT (edit)
     // Hacer el delete
     // Hacer el PUT (cambiar entre completada y pendiente)
