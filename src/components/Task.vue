e7fedb4f53ee09abd4fd8d4433eeb0bcfca6dfaf
Pierangelo Guerrero <pierangelo.guerrero@gmail.com>
2023-01-31 21:52:38 +0100

    include edit function

--- src/components/Task.vue
+++ src/components/Task.vue
@@ -1,37 +1,52 @@
 <template>
   <div
     class="bg-yellow-200 text-black py-10 px-6 rounded-lg w-1/4 focus:outline-none focus:shadow-outline ml-5 mb-5 flex justify-start flex-wrap">
+    
+    <div class=" flex flex-col-reverse">
+    
 
-    <button class="bg-slate-400 hover:bg-slate-300 text-white rounded-lg py-1 px-2 mr-8">
-      Edit
-    </button>
-
-    <button class="bg-red-600 hover:bg-red-500 text-white rounded-lg py-1 px-2">
+    <button class="bg-red-600 hover:bg-red-500 text-white rounded-lg py-1 px-2 " @click="$emit('deleteTask', task.id)">
       Delete
     </button>
 
-    <button class=" bg-blue-900 hover:bg-blue-700 text-cyan-300 rounded-lg py-1 px-2">
+    <button class="bg-slate-400 hover:bg-slate-300 text-white rounded-lg py-1 px-2 mb-4" @click="$emit('changeTask' , taskTitle, task.id )">
+      Edit
+    </button>
+
+    <button class=" bg-blue-900 hover:bg-blue-700 text-cyan-300 rounded-lg py-1 px-2 mb-4">
       Did It
     </button>
 
-    <P>{{ task.title }}</P>
+    
+    
+  </div> 
+
+  <div class=" flex flex-col">
+    <p class=" ml-10 text-lg">{{ task.title }}</p>
+  <input v-model="taskTitle" class=" ml-10 mt-5 text-lg bg-yellow-100">
+</div>
+
   </div>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import { defineComponent , ref } from 'vue'
+
+
+
 
 export default defineComponent({
     props: {
-      task: {
-          required: true
-      }
+      task: { required: true },
     },
     setup(props) {
       const { task } = props;
+      
       return { task };
     }
   });
+
+  const taskTitle = ref();
 </script>
 
 <style scoped>
--- src/store/task.js
+++ src/store/task.js
@@ -1,28 +1,40 @@
 // /store/task.js
-import { defineStore } from "pinia";
-import { supabase } from "../supabase";
+import { defineStore } from 'pinia';
+import { supabase } from '../supabase';
+const taskselector = 'tasks';
 
-export const useTaskStore = defineStore("tasks", {
+export const useTaskStore = defineStore('tasks', {
   state: () => ({
     tasks: null,
   }),
   actions: {
     async fetchTasks() {
       const { data: tasks } = await supabase
-        .from("tasks")
-        .select("*")
-        .order("id", {
+        .from(taskselector)
+        .select('*')
+        .order('id', {
           ascending: false,
         });
       this.tasks = tasks;
     },
     async insert(userId, title) {
       const { data: tasks } = await supabase
-      .from("tasks")
+      .from(taskselector)
       .insert({ user_id: userId, title })
     },
-    // Hacer el PUT (edit)
-    // Hacer el delete
+    async delete(id) {
+      await supabase
+        .from(taskselector)
+        .delete()
+        .eq('id', id);
+    },
+    async editTask(title, id) {
+      const { error } = await supabase
+        .from("tasks")
+        .update({ title: title })
+        .match({ id: id });
+      if (error) throw error;
+    },
     // Hacer el PUT (cambiar entre completada y pendiente)
   },
 });
